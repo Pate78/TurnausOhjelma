7d8c807f85d7c2fd3ec8f5de1db8ed3ed0d91f37
pitkapa7 <pitkapa7@X5CG6323YMQC0C6.tcad.telia.se>
2019-03-05 14:38:05 +0200

    1 commit

---
+++ src/main/java/turnausohjelma/turnausohjelma/TurnausOhjelma.java
@@ -0,0 +1,18 @@
+package turnausohjelma.turnausohjelma;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pitkapa7
+ */
+public class TurnausOhjelma {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        ApplicationLogic logic = new ApplicationLogic();
+    }
+}
