ebc6055aecffa55cf49f536a54a8bf303679a379
pitkapa7 <pitkapa7@X5CG6323YMQC0C6.tcad.telia.se>
2019-04-20 15:15:29 +0300

    1.Iteraatio 1.5 Iter 2.0 Iter. Working program 3.0 Iter 4.0. Iter working program with storing data into DB 4.1 Fixed goalOrderNumber vs nextGoalOrderNumber error

--- src/main/java/tournamentprogram/Game.java
+++ src/main/java/tournamentprogram/Game.java
@@ -5,89 +5,233 @@
  */
 package tournamentprogram;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Objects;
 
 /**
  * Game class which object is a floorball game where you an add goals
- * @author pitkapa7
+ * @author pate
  */
 public class Game {
+    private LocalDateTime date;
     private Team homeTeam;
     private Team visitorTeam;
     private ArrayList<Goal> homeGoals;
     private ArrayList<Goal> visitorGoals;
-    public int goalIndex;
-
-    public Game(Team homeTeam, Team vistitorTeam) {
+    private static int nextGoalOrderNumber;
+    private String gameId;
+    
+    /**
+     *
+     * @param homeTeam
+     * @param vistitorTeam
+     */
+    public Game(Team homeTeam, Team vistitorTeam, int nextGoalOrderNumber) {
         this.homeTeam = homeTeam;
         this.visitorTeam = vistitorTeam;
         this.homeGoals = new ArrayList<>();
         this.visitorGoals = new ArrayList<>();
-        this.goalIndex = 1;
+        this.nextGoalOrderNumber = nextGoalOrderNumber+1;
+        this.date = LocalDateTime.now();
+        this.gameId = Integer.toString(date.getDayOfYear()) + "." + 
+                Integer.toString(date.getMonthValue()) + "." +
+                Integer.toString(date.getYear()) + "." +
+                homeTeam.getTeamId() + "." +
+                visitorTeam.getTeamId();
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public LocalDateTime getDate(){
+        return this.date;
+    }
+    
+    /**
+     *
+     * @param id
+     */
+    public void setId(String id) {
+        this.gameId = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getGameId(){
+        return this.gameId;
+    }
+    
+    public int getNextGoalOrderNumber() {
+        return this.nextGoalOrderNumber;
     }
     
     /** Add home goal to the game functions. Own functions for both teams to clarify
      * the class
-     * @param scorer Player who did the goal
-     * @param assist1 Player who assisted the goal.
-     * @param assist2 Player who assisted the goal.
+     * @param players
      * @param time The time when the goal was done. This field can be left empty if it is not needed
      * with empty string"".
      */
-    public void addHomeGoal(Player scorer, Player assist1, Player assist2, String time) {
-        this.homeGoals.add(new Goal(scorer, assist1, assist2, time, goalIndex));
-        this.goalIndex++;
+    
+    public void addHomeGoal(ArrayList<Player> players, int nextGoalOrderNumber,  String teamId, String time) {
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId);
+        newGoal.setScorer(players.get(0));
+        if (players.size()>1) {
+            newGoal.setAssist1(players.get(1));
+        }
+        if (players.size() > 2) {
+            newGoal.setAssist2(players.get(2));
+        }
+        newGoal.setTime(time);
+        this.homeGoals.add(newGoal);
+//        System.out.println("Goal index: " + this.goalIndex);
+        this.nextGoalOrderNumber++;
+    }
+
+    /**
+     *
+     * @param scorer
+     * @param assist1
+     * @param assist2
+     * @param time
+     */
+    public void addHomeGoal(int nextGoalOrderNumber, String teamId, Player scorer, Player assist1, Player assist2, String time) {
+                System.out.println("Game: addHomeGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(gameId, nextGoalOrderNumber, teamId, scorer, assist1, assist2, time); //goalORderNumber, scorer, assist1 assist2, time, this.gameId
+        setGoalId(newGoal);
+        this.homeGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
     }
     
-    public void addHomeGoal(Player scorer, Player assist1, String time) {
-        this.homeGoals.add(new Goal(scorer, assist1, time, goalIndex));
-        this.goalIndex++;
+    /**
+     *
+     * @param scorer
+     * @param assist1
+     * @param time
+     */
+    public void addHomeGoal(int nextGoalOrderNumber, String teamId ,Player scorer, Player assist1, String time) {
+                System.out.println("Game: addHomeGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, scorer, assist1, time);
+        setGoalId(newGoal);
+        this.homeGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
     }
     
-    public void addHomeGoal(String time){
-        this.homeGoals.add(new Goal(goalIndex));
-        this.goalIndex++;
+    /**
+     *
+     * @param time
+     */
+    public void addHomeGoal(int nextGoalOrderNumber, String teamId, String time){
+                System.out.println("Game: addHomeGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, time);
+        setGoalId(newGoal);
+        this.homeGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
     }
     
-    public void addHomeGoal(Player scorer, String time) {
-        this.homeGoals.add(new Goal(scorer, time, goalIndex));
-        this.goalIndex++;
+    /**
+     *
+     * @param scorer
+     * @param time
+     */
+    public void addHomeGoal(int nextGoalOrderNumber, String teamId, Player scorer, String time) {
+                System.out.println("Game: addHomeGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, scorer, time);
+        setGoalId(newGoal);
+        this.homeGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
     }
     
     /**
      * Adding visitor goals to game.
-     * @param scorer Player who did the goal
-     * @param assist1 Player who assisted the goal
-     * @param assist2 Player who assisted the goal
+     * @param players
      * @param time Time when the goal was done
      * This field can be left empty if it is not needed
      * with empty string "".
      */
-    public void addVisitorGoal(Player scorer, Player assist1, Player assist2, String time) {
-        this.visitorGoals.add(new Goal(scorer, assist1, assist2, time, goalIndex));
-        this.goalIndex++;
+    public void addVisitorGoal(ArrayList<Player> players, int nextGoalOrderNumber, String teamId, String time) {
+        System.out.println("Game: addHomeGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, time);
+        newGoal.setScorer(players.get(0));
+        if (players.size()>1) {
+            newGoal.setAssist1(players.get(1));
+        }
+        if (players.size() > 2) {
+            newGoal.setAssist2(players.get(2));
+        }
+        this.visitorGoals.add(newGoal);
+//        System.out.println("Goal index: " + this.goalIndex);
+        this.nextGoalOrderNumber++;
     }
     
-    public void addVisitorGoal(Player scorer, Player assist1, String time) {
-        this.visitorGoals.add(new Goal(scorer, assist1, time, goalIndex));
-        this.goalIndex++;
+    /**
+     *
+     * @param scorer
+     * @param assist1
+     * @param assist2
+     * @param time
+     */
+    public void addVisitorGoal(String teamId, int nextGoalOrderNumber, Player scorer, Player assist1, Player assist2, String time) {
+        System.out.println("Game: addVisitorGoal: goalOrderNumber: " + nextGoalOrderNumber );
+        Goal newGoal = new Goal(this.gameId, nextGoalOrderNumber, teamId, scorer, assist1, assist2, time);
+        setGoalId(newGoal);
+        this.visitorGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
     }
     
-    public void addVisitorGoal(Player scorer, String time) {
-        this.visitorGoals.add(new Goal(scorer ,time, goalIndex));
-        this.goalIndex++;      
+    /**
+     *
+     * @param scorer
+     * @param assist1
+     * @param time
+     */
+    public void addVisitorGoal(int nextGoalOrderNumber, String teamId, Player scorer, Player assist1, String time) {
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, scorer, assist1, time);
+        System.out.println("Game: addVisitorGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        setGoalId(newGoal);
+        this.visitorGoals.add(newGoal);
+        this.nextGoalOrderNumber++;;
     }
     
-    public void addVisitorGoal(String time){
-        this.visitorGoals.add(new Goal(goalIndex));
-        this.goalIndex++;
+    /**
+     *
+     * @param scorer
+     * @param time
+     */
+    public void addVisitorGoal(int nextGoalOrderNumber, String teamId, Player scorer, String time) {
+        System.out.println("Game: addVisitorGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, scorer, time);
+        setGoalId(newGoal);
+        this.visitorGoals.add(newGoal);
+        this.nextGoalOrderNumber++;   
     }
     
+    /**
+     *
+     * @param time
+     */
+    public void addVisitorGoal(int nextGoalOrderNumber, String teamId, String time){
+        System.out.println("Game: addVisitorGoal: goalOrderNumber: " + nextGoalOrderNumber );
+
+        Goal newGoal = new Goal(nextGoalOrderNumber, teamId, time);
+        setGoalId(newGoal);
+        this.visitorGoals.add(newGoal);
+        this.nextGoalOrderNumber++;
+    }
     
+    private void setGoalId(Goal goal) {
+        goal.setGoalId(nextGoalOrderNumber, this.gameId);
+    }
     
     /**Remove specific goal from game
      * THIS NEEDS TO BE IMPLEMENTED
@@ -117,26 +261,50 @@ public class Game {
         }
     }
     
+    /**
+     *
+     * @param t
+     */
     public void setHomeTeam(Team t) {
         this.homeTeam = t;
     }
 
+    /**
+     *
+     * @param t
+     */
     public void setVisitorTeam(Team t) {
         this.visitorTeam = t;
     }
     
+    /**
+     *
+     * @return
+     */
     public ArrayList<Goal> getHomeGoals() {
         return homeGoals;
     }
     
+    /**
+     *
+     * @return
+     */
     public ArrayList<Goal> getVisitorGoals() {
         return visitorGoals;
     }
 
+    /**
+     *
+     * @return
+     */
     public Team getHomeTeam() {
         return homeTeam;
     }
 
+    /**
+     *
+     * @return
+     */
     public Team getVisitorTeam() {
         return visitorTeam;
     }
